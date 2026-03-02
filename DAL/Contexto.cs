d1b337ef020aad1804a98d49810a29ab277e0b90
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-03-02 18:29:05 -0400

    Crea carpetas y agrega el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Raydelis_HilarioAP1_P2.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
