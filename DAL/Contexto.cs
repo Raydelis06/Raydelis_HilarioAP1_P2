d12ef9b93d3e3b7885de3afa623f65d5e077026c
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-03-09 18:38:14 -0400

    Modifica el contexto incluyento propiedades y objetos en el modelcreating

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,13 +1,88 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Raydelis_HilarioAP1_P2.Models;
 
 namespace Raydelis_HilarioAP1_P2.DAL;
 
 public class Contexto : DbContext
 {
-    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+
+    {
+
+    }
+
+    public DbSet<Estudiantes> Estudiantes { get; set; }
+    public DbSet<TiposPuntos> TiposPuntos { get; set; }
+    public DbSet<AsignacionesPuntos> AsignacionesPuntos { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Estudiantes>().HasData(
+
+            new Estudiantes
+            {
+                EstudianteId = 1,
+                Nombres = "Ana Martínez",
+                Email = "ana@universidad.edu",
+                Edad = 20,
+                BalancePuntos = 0
+            },
+
+            new Estudiantes
+            {
+                EstudianteId = 2,
+                Nombres = "Carlos Pérez",
+                Email = "carlos@universidad.edu",
+                Edad = 22,
+                BalancePuntos = 0
+            },
+
+            new Estudiantes
+            {
+                EstudianteId = 3,
+                Nombres = "Laura Rodríguez",
+                Email = "laura@universidad.edu",
+                Edad = 21,
+                BalancePuntos = 0
+            }
+        );
+
+        modelBuilder.Entity<TiposPuntos>().HasData(
+
+            new TiposPuntos
+            {
+                TipoId = 1,
+                Nombre = "Participación",
+                Descripcion = "Participación en clase",
+                ValorPuntos = 5,
+                Color = "primary",
+                Icono = "bi-hand-thumbs-up",
+                Activo = true
+            },
+
+            new TiposPuntos
+            {
+                TipoId = 2,
+                Nombre = "Tarea Entregada",
+                Descripcion = "Entrega de tarea",
+                ValorPuntos = 10,
+                Color = "success",
+                Icono = "bi-journal-check",
+                Activo = true
+            },
+
+            new TiposPuntos
+            {
+                TipoId = 3,
+                Nombre = "Proyecto",
+                Descripcion = "Entrega de proyecto",
+                ValorPuntos = 20,
+                Color = "warning",
+                Icono = "bi-lightbulb",
+                Activo = true
+            }
+        );
     }
 }
