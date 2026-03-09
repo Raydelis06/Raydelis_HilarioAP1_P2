7d94a24d10cc04aae1c12c70d6c8d220569c9c6d
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-03-09 18:34:10 -0400

    Agrega el modelo TiposPuntos

---
+++ Models/TiposPuntos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Raydelis_HilarioAP1_P2.Models
+{
+    public class TiposPuntos
+    {
+        [Key]
+
+        public int TipoId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este Campo Es Requerido")]
+        public string Descripcion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este Campo Es Requerido")]
+        public int ValorPuntos { get; set; }
+
+        [Required(ErrorMessage = "Este Campo Es Requerido")]
+        public string Color { get; set; }
+
+        [Required(ErrorMessage = "Este Campo Es Requerido")]
+        public string Icono { get; set; }
+
+        [Required(ErrorMessage = "Este Campo Es Requerido")]
+        public bool Activo { get; set; } = true;
+    }
+}
