2da7f0258d96cc3d0c70a50e97d973dd34a65262
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-03-09 18:34:48 -0400

    Agrega el modelo AsignacionesPuntos

---
+++ Models/AsignacionesPuntos.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Raydelis_HilarioAP1_P2.Models
+{
+    public class AsignacionesPuntos
+    {
+        [Key]
+        public int IdAsignacion { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        [Range(1, int.MaxValue, ErrorMessage = "Este Campo es obligatorio")]
+        public int EstudianteId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Este Campo es obligatorio")]
+        public int TotalPuntos { get; set; } = 0;
+
+        [ForeignKey("IdAsignacion")]
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        public ICollection<DetalleAsignacionPuntos> DetallesAsignacion { get; set; } = new List<DetalleAsignacionPuntos>();
+    }
+}
